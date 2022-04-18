851f209669df7ac8d54341f9e931d1e83035cbda
Rodrigo Chagas <96203034+RodrigodevChagas@users.noreply.github.com>
2022-04-18 16:48:35 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaLogin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
